0f129a5313409d373b934d915acd85bcee0b4419
Alberto Hitiro Tsugawa <hitiro@live.com>
2023-01-12 10:54:58 -0300

    feat: buscando dados na api com async await

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa'
+import {
+  Container,
+  Form,
+  SubmitButton
+} from './styles';
 
-import { Title } from './styles';
+import api from '../../services/api'
 
 export default function Main() {
+
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorios, setRepositorios] = useState([]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    }
+
+    setRepositorios([...repositorios, data]);
+    setNewRepo('');
+
+    console.log(repositorios);
+
+  }
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
   return (
-    <Title>
-      Main
-    </Title>
+    <Container>
+
+      <h1>
+        <FaGithub size={25} />
+        Meus repositórios
+      </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar Repositórios"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+
+    </Container>
   )
 }
