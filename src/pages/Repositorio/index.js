d3bcd037260f4ac0316c1adba083f41f5cf3b73d
Alberto Hitiro Tsugawa <hitiro@live.com>
2023-01-13 12:37:15 -0300

    feat: listando issues do repositorio

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,11 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList
+} from './styles';
+import { FaArrowLeft } from 'react-icons/fa';
 
 export default function Repositorio({ match }) {
-  return (
-    <h1 style={{ color: '#fff' }}>
-      Repositorio <br />
 
-      {decodeURIComponent(match.params.repositorio)}
-    </h1>
+  const [repositorio, setRepositorio] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    async function load() {
+      const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+
+      setRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+
+      console.log(repositorioData.data);
+      console.log(issuesData.data);
+
+    };
+
+    load();
+
+  }, [match.params.repositorio]);
+
+  if (loading) {
+    return (
+      <Loading>
+        <h1>Carregando</h1>
+      </Loading>
+    )
+  }
+
+  return (
+    <Container>
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </BackButton>
+
+      <Owner>
+        <img
+          src={repositorio.owner.avatar_url}
+          alt={repositorio.owner.login}
+        />
+        <h1>{repositorio.name}</h1>
+        <p> {repositorio.description} </p>
+      </Owner>
+
+      <IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+          </li>
+        ))}
+      </IssuesList>
+
+    </Container>
   );
 }
